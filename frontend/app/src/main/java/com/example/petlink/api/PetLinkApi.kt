b5de72ead176604d98bef0a425dd97e277ff01fe
Aleksandr <Aleksandr260206@yandex.ru>
2025-09-28 18:16:04 +0300

    add some req

--- frontend/app/src/main/java/com/example/petlink/api/PetLinkApi.kt
+++ frontend/app/src/main/java/com/example/petlink/api/PetLinkApi.kt
@@ -1,10 +1,14 @@
 package com.example.petlink.api
 
+import com.example.petlink.data.model.AnimalPhotoReq
 import com.example.petlink.data.model.AuthResponse
 import com.example.petlink.data.model.LoginRequest
 import com.example.petlink.data.model.RegistrationRequest
+import com.example.petlink.data.model.AnimalReq
+import com.example.petlink.data.model.StatusReq
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 
@@ -14,5 +18,14 @@ interface PetLinkApi {
 
     @POST("api-token-auth/")
     fun login(@Body loginRequest: LoginRequest): Call<AuthResponse>
+
+    @GET("api/animals/")
+    fun getAnimals(): Call<List<AnimalReq>>
+
+    @GET("api/animal_photos/")
+    fun getAnimalPhotos(): Call<List<AnimalPhotoReq>>
+
+    @GET("api/statuses/")
+    fun getStatuses(): Call<List<StatusReq>>
 
 }
